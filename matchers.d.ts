903dcefadaa41347c4491f712c6d48ecbd201b4b
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-10-17 16:40:56 +0000

    some formatting changes

--- matchers.d.ts
+++ matchers.d.ts
@@ -1,8 +1,7 @@
-import { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-import { Matchers, AsymmetricMatchers } from 'bun:test';
+import { AsymmetricMatchers, Matchers } from "bun:test"
+import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
 
-declare module 'bun:test' {
-  interface Matchers<T>
-    extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
-  interface AsymmetricMatchers extends TestingLibraryMatchers {}
+declare module "bun:test" {
+	interface Matchers<T> extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
+	interface AsymmetricMatchers extends TestingLibraryMatchers {}
 }
