5f3dcb5ac0aed8b96f661282af7099623d1fb892
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-10-17 00:02:39 +0000

    attempt to use testing library

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,17 @@
 import { describe, it, expect } from "bun:test"
+import page from "./routes/+page.svelte"
+import { screen, render } from "@testing-library/svelte"
 
 describe("sum test", () => {
 	it("adds 1 + 2 to equal 3", () => {
 		expect(1 + 2).toBe(3)
+	})
+})
+
+describe("root page test", () => {
+	it("has the correct header", async () => {
+		render(page)
+		const header = await screen.findByRole("heading")
+		expect(header).toBeInTheDocument()
 	})
 })
