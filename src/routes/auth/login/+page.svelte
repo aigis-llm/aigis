649a659c0ef9833f928fde472cec96f8e465935b
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-11-10 22:22:53 +0000

    it works!

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div class="h-screen flex flex-col items-center justify-center">
+	<h1>Login</h1>
 	<form method="POST">
 		<label class="flex flex-row w-full py-2">
 			<p class="flex-1 text-center px-2">Email</p>
@@ -10,7 +11,7 @@
 				name="email"
 				type="email"
 				autocomplete="username"
-				class="flex-initial border-2 border-solid border-[--ctp-surface1] rounded-lg color-[--ctp-{$preferences.theme}-text] bg-[--ctp-{$preferences.theme}-surface0]"
+				class="flex-initial border-2 border-solid border-[--ctp-surface1] rounded-lg color-[--ctp-text] bg-[--ctp-surface0]"
 			/>
 		</label>
 		<label class="flex flex-row w-full py-2">
@@ -19,12 +20,12 @@
 				name="password"
 				type="password"
 				autocomplete="current-password"
-				class="flex-initial border-0 rounded-lg color-ctp-{$preferences.theme}-text bg-ctp-{$preferences.theme}-surface0"
+				class="flex-initial border-2 border-solid border-[--ctp-surface1] rounded-lg color-[--ctp-text] bg-[--ctp-surface0]"
 			/>
 		</label>
 		<div class="flex flex-row justify-center">
 			<button
-				class="w-24 border-0 rounded-lg color-ctp-{$preferences.theme}-text bg-ctp-{$preferences.theme}-surface0"
+				class="w-24 border-0 rounded-lg color-[--ctp-text] bg-[--ctp-surface0]"
 				>Log in</button
 			>
 		</div>
--- uno.config.ts
+++ uno.config.ts
@@ -7,10 +7,48 @@ import {
 	transformerDirectives,
 } from "unocss"
 import extractorSvelte from "@unocss/extractor-svelte"
+import * as cssTree from "css-tree"
+import { flavorEntries } from "@catppuccin/palette"
 
 export default defineConfig({
 	extractors: [extractorSvelte()],
 	transformers: [transformerDirectives()],
+	preflights: [
+		{
+			getCSS: () => {
+				const ast = cssTree.parse("") as cssTree.StyleSheet
+				for (const flavor of flavorEntries) {
+					const flavorRule: cssTree.Rule = {
+						type: "Rule",
+						prelude: {
+							type: "SelectorList",
+							children: new cssTree.List<cssTree.CssNode>().appendData({
+								type: "Selector",
+								children: new cssTree.List<cssTree.CssNode>().appendData({
+									type: "ClassSelector",
+									name: `theme-${flavor[0]}`,
+								}),
+							}),
+						},
+						block: {
+							type: "Block",
+							children: new cssTree.List(),
+						},
+					}
+					for (const color of flavor[1].colorEntries) {
+						flavorRule.block.children.push({
+							type: "Declaration",
+							property: `--ctp-${color[0]}`,
+							value: { type: "Raw", value: `hsl(var(--ctp-${flavor[0]}-${color[0]}-hsl))` },
+							important: false,
+						})
+					}
+					ast.children.push(flavorRule)
+				}
+				return cssTree.generate(ast as cssTree.CssNode)
+			},
+		},
+	],
 	presets: [
 		presetUno(),
 		presetIcons(),
@@ -23,7 +61,5 @@ export default defineConfig({
 		}),
 		presetTypography(),
 	],
-	safelist: [
-		"font-sans",
-	],
+	safelist: ["font-sans"],
 })
