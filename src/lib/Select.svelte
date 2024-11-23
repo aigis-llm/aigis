2d1594c66eb2507ff69d64b8fbe4ff7a880053ff
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-11-23 21:53:06 +0000

    idk what the error im getting means

--- src/lib/Select.svelte
+++ src/lib/Select.svelte
@@ -5,10 +5,18 @@
 		options = $bindable([]),
 		selected = $bindable({ value: "ERROR", label: "ERROR" }),
 		class: className = $bindable(""),
+		button_label_class = $bindable(""),
+		button_icon_class = $bindable(""),
+		select_div_class = $bindable(""),
+		select_option_class = $bindable(""),
 	}: {
 		options: Array<{ value: string; label: string }>
 		selected: { value: string; label: string }
-		class: string
+		class: string | undefined
+		button_label_class: string | undefined
+		button_icon_class: string | undefined
+		select_div_class: string | undefined
+		select_option_class: string | undefined
 	} = $props()
 
 	const {
@@ -35,17 +43,19 @@
 	class="flex flex-row items-center rounded-md cursor-pointer border-2 border-solid border-[--ctp-surface1] color-[--ctp-text] bg-[--ctp-surface0] {className}"
 	use:melt={$trigger}
 >
-	<div class="flex-1 text-start">{$selectedLabel}</div>
-	<div class="i-tabler:chevron-down flex-initial w-1em h-1em inline-block"></div>
+	<div class="flex-1 text-start {button_label_class}">{$selectedLabel}</div>
+	<div
+		class="i-tabler:chevron-down flex-initial w-1em h-1em inline-block {button_icon_class}"
+	></div>
 </button>
 {#if $open}
 	<div
-		class="z-10 flex max-h-[300px] flex-col overflow-y-auto rounded-lg bg-[--ctp-surface0] p-1"
+		class="z-10 flex max-h-[300px] flex-col overflow-y-auto rounded-lg bg-[--ctp-surface0] p-1 {select_div_class}"
 		use:melt={$menu}
 	>
 		{#each options as item}
 			<div
-				class="rounded-lg p-1 data-[highlighted]:bg-[--ctp-surface1] !aria-selected:bg-[--ctp-surface2] cursor-pointer"
+				class="rounded-lg p-1 data-[highlighted]:bg-[--ctp-surface1] !aria-selected:bg-[--ctp-surface2] cursor-pointer {select_option_class}"
 				use:melt={$option(item)}
 			>
 				{item.label}
