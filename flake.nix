2a416f2a02f9067258fd688571ec1e248a29a037
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-12-04 21:57:37 +0000

    refactor: Migrate to `uv` and `uv2nix` from Rye (#28)
    [`uv2nix`](https://github.com/pyproject-nix/uv2nix) works better than
    using Rye with Nix because you do not have to run `just
    register-toolchain` or use other hacks because we can set `UV_PYTHON`
    and let `uv` and `uv2nix` take care of the rest.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,41 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix-hammer-overrides = {
+      url = "github:TyberiusPrime/uv2nix_hammer_overrides";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{
       nixpkgs,
       flake-parts,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      uv2nix-hammer-overrides,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -34,46 +62,58 @@
               pkgs = import nixpkgs {
                 inherit system;
               };
-              pythonldpath = lib.makeLibraryPath (
-                with pkgs;
-                [
-                  zlib
-                  zstd
-                  stdenv.cc.cc.lib
-                  curl
-                  openssl
-                  attr
-                  libssh
-                  bzip2
-                  libxml2
-                  acl
-                  libsodium
-                  util-linux
-                  xz
-                  systemd
-                ]
-              );
-              wrapPrefix = if (!pkgs.stdenv.isDarwin) then "LD_LIBRARY_PATH" else "DYLD_LIBRARY_PATH";
-              patchedPython = (
-                pkgs.symlinkJoin {
-                  name = "python-patched";
-                  paths = [ pkgs.python313 ];
-                  buildInputs = [ pkgs.makeWrapper ];
-                  postBuild = ''
-                    wrapProgram "$out/bin/python3.13" --prefix ${wrapPrefix} : "${pythonldpath}"
-                  '';
+              workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+              overlay = workspace.mkPyprojectOverlay {
+                sourcePreference = "wheel";
+              };
+              pyprojectOverrides = pkgs.lib.composeExtensions (uv2nix-hammer-overrides.overrides_strict pkgs) (
+                _final: prev: {
+
                 }
               );
+              editableOverlay = workspace.mkEditablePyprojectOverlay {
+                # Use environment variable
+                root = "$REPO_ROOT";
+              };
+              python = pkgs.python313;
+              pythonSet =
+                # Use base package set from pyproject.nix builders
+                (pkgs.callPackage pyproject-nix.build.packages {
+                  inherit python;
+                }).overrideScope
+                  (
+                    lib.composeManyExtensions [
+                      pyproject-build-systems.overlays.default
+                      overlay
+                      pyprojectOverrides
+                    ]
+                  );
+
+              editablePythonSet = pythonSet.overrideScope editableOverlay;
+
+              # Build virtual environment, with local packages being editable.
+              #
+              # Enable all optional dependencies for development.
+              virtualenv = editablePythonSet.mkVirtualEnv "aigis-dev-env" workspace.deps.all;
             in
             pkgs.mkShell {
               packages = [
                 pkgs.nixfmt-rfc-style
-                patchedPython
-                pkgs.rye
+                virtualenv
+                pkgs.uv
                 pkgs.bun
                 pkgs.just
                 pkgs.supabase-cli
               ];
+
+              shellHook = ''
+                # Undo dependency propagation by nixpkgs.
+                unset PYTHONPATH
+                # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
+                export REPO_ROOT=$(git rev-parse --show-toplevel)
+                # Make uv use our Python.
+                export UV_PYTHON=$(which python)
+              '';
             };
         };
     };
