94dcd3b0adeab98a40a73ccbdbbc535fdd7a1a85
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-12-09 21:23:52 +0000

    chore(tests): Replace a patche with --conditions=browser
    Using `--conditions=browser` with Bun makes it import client-sided
    libraries instead of server-sided libraries, namely the Svelte runtime.

--- patch.sh
+++ patch.sh
@@ -8,14 +8,10 @@ rm -rf node_modules/@testing-library/svelte/ vendor-node/@testing-library/svelte
 bun install
 cp -r node_modules/css-tree/ vendor-node/
 patch -p0 -d . -i css-tree-bun.patch
-cp -r node_modules/svelte/ vendor-node/
-patch -p0 -d . -i svelte-client.patch
 cp -r node_modules/@testing-library/svelte/ vendor-node/@testing-library/svelte/
 patch -p0 -d . -i testing-library-svelte.patch
 # HORRIBLE HACK BECAUSE BUN DOES NOT SUPPORT NESTED DEPENDENCY OVERRIDES https://github.com/oven-sh/bun/issues/6608
 rm -rf node_modules/css-tree
 cp -r vendor-node/css-tree/ node_modules/
-rm -rf node_modules/svelte
-cp -r vendor-node/svelte/ node_modules/
 rm -rf node_modules/@testing-library/svelte
 cp -r vendor-node/@testing-library/svelte node_modules/@testing-library
