bb6bd7768312bacf62b1e855b07ec6734e5346e4
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-01-21 18:56:30 +0000

    feat(tests): Add CSS styles to the test environment

--- src/lib/navbar.test.ts
+++ src/lib/navbar.test.ts
@@ -1,9 +1,10 @@
 import { test, expect } from "vitest"
 import Navbar from "$lib/Navbar.svelte"
-import { screen, render } from "@testing-library/svelte"
+import { screen } from "@testing-library/svelte"
+import { render } from "$lib/test_utils"
 
-test("Navbar exists", () => {
-	render(Navbar)
+test("Navbar exists", async () => {
+	await render(Navbar)
 	const myComponent = screen.getByRole("navigation")
 	expect(myComponent).toBeInTheDocument()
 })
