58a5614b2d1b10b74e050fab78dd994ee1eff850
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-01-21 21:57:45 +0000

    tests(select): Add Select tests

---
+++ src/lib/Select.test.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Select from "$lib/Select.svelte"
+
+	let selected = $state({
+		value: "alice",
+		label: "Alice",
+	})
+
+	const options = [
+		{ value: "alice", label: "Alice" },
+		{ value: "bob", label: "Bob" },
+	]
+</script>
+
+<Select {options} bind:selected>Name:</Select>
