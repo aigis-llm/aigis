434b9752ab0cd0f902b380492e0e7e627c18b6e2
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-01-22 22:56:35 +0000

    chore: Add coverage to eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,6 @@ export default [
 		},
 	},
 	{
-		ignores: ["build/", ".svelte-kit/", "dist/", "vendor-node/"],
+		ignores: ["build/", ".svelte-kit/", "dist/", "vendor-node/", "coverage/"],
 	},
 ]
